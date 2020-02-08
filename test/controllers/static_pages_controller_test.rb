9d48f6211ab801d3c9c83763e7ed10bf050d1a81
davidauza.engineer <davidauza01@gmail.com>
2020-02-08 01:20:19 -0500

    Add header partial

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  # TODO test home
   test "should get home" do
     get root_url
     assert_response :success
